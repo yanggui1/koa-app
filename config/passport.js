1c0f714cd8ea80437582ff113437adec2a77a7d6
yanggui2 <yanggui2@lenovo.com>
2019-04-16 18:30:04 +0800

    update

--- config/passport.js
+++ config/passport.js
@@ -3,28 +3,28 @@ const JwtStrategy = require("passport-jwt").Strategy,
 const keys = require("../config/keys");
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKeys = keys.secretOrKeys;
+opts.secretOrKey = keys.secretOrKeys;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
 
 module.exports = passport => {
-    // passport.use(
-    //     new JwtStrategy(opts, async function(jwt_payload, done) {
+    passport.use(
+        new JwtStrategy(opts, async function(jwt_payload, done) {
 
-    //         console.log(jwt_payload);
-    //         // User.findOne({id: jwt_payload.sub}, function(err, user){
-    //         //     if(err) {
-    //         //         return done(err,false);
-    //         //     }
-    //         //     if(user) {
-    //         //         return done(null, user);
-    //         //     }else {
-    //         //         return done(null, false);
-    //         //         // or yuo could create a new account
-    //         //     }
-    //         // });
-    //     })
-    // );
+            console.log(jwt_payload);
+            // User.findOne({id: jwt_payload.sub}, function(err, user){
+            //     if(err) {
+            //         return done(err,false);
+            //     }
+            //     if(user) {
+            //         return done(null, user);
+            //     }else {
+            //         return done(null, false);
+            //         // or yuo could create a new account
+            //     }
+            // });
+        })
+    );
     // console.log(passport)
 
 };
